e4e72211bfc5104534a447526ede5d27b007af58
ajayalways26 <ajay.singh1@brillio.com>
2025-08-19 18:14:51 +0530

    File adam_data_engg_fsi_demo_product.product_status.sql modified successfully

--- docs/ddl_mysql_snowflake/adam_data_engg_fsi_demo_product.product_status.sql
+++ docs/ddl_mysql_snowflake/adam_data_engg_fsi_demo_product.product_status.sql
@@ -1,13 +1,13 @@
 CREATE OR REPLACE TABLE adam_data_engg_fsi_demo.adam_data_engg_fsi_demo_product.product_status (
-    product_status_id NUMBER(19,0) NOT NULL COMMENT 'Unique Identifier',
-    product_status_code VARCHAR(16) NOT NULL COMMENT 'Status code (e.g., ACTIVE, INACTIVE)',
-    product_status_name VARCHAR(32) NOT NULL COMMENT 'Status Code Name',
-    product_status_description VARCHAR(64) COMMENT 'Human-readable status',
-    created_by VARCHAR(64) NOT NULL COMMENT 'Record created by',
-    created_at TIMESTAMP_LTZ NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT 'Record creation timestamp in UTC',
-    updated_by VARCHAR(64) COMMENT 'Record updated by',
-    updated_at TIMESTAMP_LTZ DEFAULT CURRENT_TIMESTAMP COMMENT 'Record last updated timestamp in UTC',
+    product_status_id        NUMBER(19,0)        NOT NULL COMMENT 'Unique Identifier',
+    product_status_code      VARCHAR(16)         NOT NULL COMMENT 'Status code (e.g., ACTIVE, INACTIVE)',
+    product_status_name      VARCHAR(32)         NOT NULL COMMENT 'Status Code Name',
+    product_status_description VARCHAR(64)       COMMENT 'Human-readable status',
+    created_by               VARCHAR(64)         NOT NULL COMMENT 'Record created by',
+    created_at               TIMESTAMP_LTZ       NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT 'Record creation timestamp in UTC',
+    updated_by               VARCHAR(64)         COMMENT 'Record updated by',
+    updated_at               TIMESTAMP_LTZ       DEFAULT CURRENT_TIMESTAMP COMMENT 'Record last updated timestamp in UTC',
 
     CONSTRAINT PK_PRODUCT_STATUS PRIMARY KEY (product_status_id)
 )
-COMMENT='Master table for product status codes';
+COMMENT = 'Master table for product status codes';
