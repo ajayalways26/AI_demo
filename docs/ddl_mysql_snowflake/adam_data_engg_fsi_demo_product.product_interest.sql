9b81961d1a0ccda322c547aa141bcae3bdfe95b0
ajayalways26 <ajay.singh1@brillio.com>
2025-08-19 17:59:25 +0530

    File adam_data_engg_fsi_demo_product.product_interest.sql create successfully

---
+++ docs/ddl_mysql_snowflake/adam_data_engg_fsi_demo_product.product_interest.sql
@@ -0,0 +1,16 @@
+CREATE OR REPLACE TABLE adam_data_engg_fsi_demo.adam_data_engg_fsi_demo_product.product_interest (
+    interest_id NUMBER(19,0) NOT NULL COMMENT 'Unique identifier for interest',
+    product_id NUMBER(19,0) NOT NULL COMMENT 'Reference to Product',
+    interest_rate NUMBER(5,2) NOT NULL COMMENT 'Interest rate (e.g., 5.25)',
+    interest_type VARCHAR(255) NOT NULL COMMENT 'Interest Type',
+    effective_from DATE NOT NULL COMMENT 'Effective from date from which the interest is applied',
+    effective_to DATE COMMENT 'Effective TO date until which the interest is applied',
+    created_by VARCHAR(64) NOT NULL COMMENT 'Record created by',
+    created_at TIMESTAMP_LTZ DEFAULT CURRENT_TIMESTAMP NOT NULL COMMENT 'Record creation timestamp in UTC',
+    updated_by VARCHAR(64) COMMENT 'Record last updated by',
+    updated_at TIMESTAMP_LTZ DEFAULT CURRENT_TIMESTAMP COMMENT 'Record last updated timestamp in UTC',
+
+    CONSTRAINT PK_PRODUCT_INTEREST PRIMARY KEY (interest_id),
+    CONSTRAINT FK_INTEREST_PRODUCT FOREIGN KEY (product_id) REFERENCES adam_data_engg_fsi_demo.adam_data_engg_fsi_demo_product.product (product_id)
+)
+COMMENT = 'Interest rates applicable to products';
