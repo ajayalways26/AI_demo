33ff012cc1b0906dc0032acf9ca9229cc2497f60
ajayalways26 <ajay.singh1@brillio.com>
2025-08-19 18:13:54 +0530

    File adam_data_engg_fsi_demo_product.product.sql modified successfully

--- docs/ddl_mysql_snowflake/adam_data_engg_fsi_demo_product.product.sql
+++ docs/ddl_mysql_snowflake/adam_data_engg_fsi_demo_product.product.sql
@@ -8,11 +8,11 @@ CREATE OR REPLACE TABLE adam_data_engg_fsi_demo.adam_data_engg_fsi_demo_product.
     status_id NUMBER(19,0) NOT NULL COMMENT 'Reference to the product status',
     created_by VARCHAR(64) NOT NULL COMMENT 'Record created by',
     created_at TIMESTAMP_LTZ DEFAULT CURRENT_TIMESTAMP NOT NULL COMMENT 'Record creation timestamp in UTC',
-    updated_by VARCHAR(64) NULL COMMENT 'Record last updated by',
-    updated_at TIMESTAMP_LTZ DEFAULT CURRENT_TIMESTAMP NULL COMMENT 'Record last updated timestamp in UTC',
-
-    CONSTRAINT PK_PRODUCT PRIMARY KEY (product_id),
-    CONSTRAINT FK_PRODUCT_CATEGORY FOREIGN KEY (product_category_id) REFERENCES adam_data_engg_fsi_demo.adam_data_engg_fsi_demo_product.product_category (product_category_id),
-    CONSTRAINT FK_PRODUCT_STATUS FOREIGN KEY (status_id) REFERENCES adam_data_engg_fsi_demo.adam_data_engg_fsi_demo_product.product_status (product_status_id),
-    CONSTRAINT FK_PRODUCT_TYPE FOREIGN KEY (product_type_id) REFERENCES adam_data_engg_fsi_demo.adam_data_engg_fsi_demo_product.product_type (product_type_id)
-) COMMENT='Product master table';
+    updated_by VARCHAR(64) COMMENT 'Record last updated by',
+    updated_at TIMESTAMP_LTZ COMMENT 'Record last updated timestamp in UTC',
+    CONSTRAINT PK_product PRIMARY KEY (product_id),
+    CONSTRAINT FK_product_type FOREIGN KEY (product_type_id) REFERENCES adam_data_engg_fsi_demo.adam_data_engg_fsi_demo_product.product_type (product_type_id),
+    CONSTRAINT FK_product_category FOREIGN KEY (product_category_id) REFERENCES adam_data_engg_fsi_demo.adam_data_engg_fsi_demo_product.product_category (product_category_id),
+    CONSTRAINT FK_product_status FOREIGN KEY (status_id) REFERENCES adam_data_engg_fsi_demo.adam_data_engg_fsi_demo_product.product_status (product_status_id)
+)
+COMMENT = 'Product master table';
