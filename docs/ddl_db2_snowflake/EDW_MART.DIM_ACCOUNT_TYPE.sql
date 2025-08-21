b8fdb1d78669a93916c114b4b3e710c515831f51
ajayalways26 <ajay.singh1@brillio.com>
2025-08-21 21:45:51 +0530

    File EDW_MART.DIM_ACCOUNT_TYPE.sql create successfully

---
+++ docs/ddl_db2_snowflake/EDW_MART.DIM_ACCOUNT_TYPE.sql
@@ -0,0 +1,25 @@
+CREATE OR REPLACE TABLE adam_data_engg_fsi_demo.EDW_MART.DIM_ACCOUNT_TYPE (
+    SK_ACCOUNT_TYPE_KEY BIGINT NOT NULL COMMENT 'Primary key of the account type dimension',
+    ACCOUNT_TYPE_CODE VARCHAR(64) COMMENT 'Code representing the type of account',
+    ACCOUNT_TYPE_ID BIGINT COMMENT 'Identifier for the account type',
+    ACCOUNT_TYPE_NAME VARCHAR(128) COMMENT 'Name of the account type',
+    ALLOWS_OVERDRAFT BOOLEAN COMMENT 'Indicates whether overdraft is allowed',
+    APPLICABLE_FOR VARCHAR(32) COMMENT 'Applicable category for this account type',
+    BUSINESS_COLUMN VARCHAR(64) COMMENT 'Business-specific column description',
+    CREATED_BY VARCHAR(64) COMMENT 'User who created the record',
+    CREATE_BATCH_ID BIGINT COMMENT 'Batch identifier for creation',
+    CREATE_DATE_TIME TIMESTAMP_NTZ COMMENT 'Timestamp of record creation',
+    CURRENT_FLAG CHAR(1) COMMENT 'Indicates whether the record is current',
+    DATA_SOURCE VARCHAR(64) COMMENT 'Source of input data for the record',
+    DESCRIPTION VARCHAR(255) COMMENT 'Description of the account type',
+    EFFECTIVE_FROM TIMESTAMP_NTZ NOT NULL COMMENT 'Start of the effective validity period',
+    EFFECTIVE_TO TIMESTAMP_NTZ COMMENT 'End of the effective validity period',
+    IS_DELETED CHAR(1) COMMENT 'Indicates whether the record is marked as deleted',
+    IS_INTEREST_BEARING BOOLEAN COMMENT 'Indicates whether the account type is interest-bearing',
+    STATUS_CODE VARCHAR(16) COMMENT 'Status code for the account type',
+    UPDATED_BY VARCHAR(64) COMMENT 'User who last updated the record',
+    UPDATE_BATCH_ID BIGINT COMMENT 'Batch identifier for the last update',
+    UPDATE_DATE_TIME TIMESTAMP_NTZ COMMENT 'Timestamp of the last update',
+
+    CONSTRAINT PK_DIM_ACCOUNT_TYPE PRIMARY KEY (SK_ACCOUNT_TYPE_KEY)
+) COMMENT 'Dimension table for account types';
