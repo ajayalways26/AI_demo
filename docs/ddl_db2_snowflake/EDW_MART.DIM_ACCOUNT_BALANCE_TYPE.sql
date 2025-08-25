07fc33cbe82103391792da11cdc9c0a6339990f9
ajayalways26 <ajay.singh1@brillio.com>
2025-08-25 12:59:46 +0530

    File EDW_MART.DIM_ACCOUNT_BALANCE_TYPE.sql modified successfully

--- docs/ddl_db2_snowflake/EDW_MART.DIM_ACCOUNT_BALANCE_TYPE.sql
+++ docs/ddl_db2_snowflake/EDW_MART.DIM_ACCOUNT_BALANCE_TYPE.sql
@@ -1,23 +1,22 @@
 CREATE OR REPLACE TABLE adam_data_engg_fsi_demo.EDW_MART.DIM_ACCOUNT_BALANCE_TYPE (
-  ACCOUNT_BALANCE_TYPE_ID   BIGINT                      COMMENT 'Unique identifier for the account balance type',
-  BALANCE_TYPE_CODE         VARCHAR(64)                COMMENT 'Code representing the balance type',
-  BALANCE_TYPE_NAME         VARCHAR(128)               COMMENT 'Name of the balance type',
-  BUSINESS_COLUMN           VARCHAR(64),
-  CREATED_BY                VARCHAR(64)                COMMENT 'User who created the record',
-  CREATE_BATCH_ID           BIGINT                     COMMENT 'Batch ID associated with the creation',
-  CREATE_DATE_TIME          TIMESTAMP_NTZ              COMMENT 'Timestamp when the record was created',
-  CURRENT_FLAG              CHAR(1)                   COMMENT 'Indicates if the record is current (Y/N)',
-  DATA_SOURCE               VARCHAR(64)                COMMENT 'Source system of the data',
-  DESCRIPTION               VARCHAR(255)              COMMENT 'Description or additional details',
-  EFFECTIVE_FROM            TIMESTAMP_NTZ NOT NULL     COMMENT 'Start date from which the record is effective',
-  EFFECTIVE_TO              TIMESTAMP_NTZ              COMMENT 'End date until which the record is effective',
-  IS_DELETED                CHAR(1)                   COMMENT 'Flag indicating if the record is deleted (Y/N)',
-  SK_ACCOUNT_BALANCE_TYPE_KEY BIGINT NOT NULL          COMMENT 'Surrogate key for the account balance type',
-  STATUS_CODE               VARCHAR(16)               COMMENT 'Code indicating the status',
-  UPDATED_BY                VARCHAR(64)               COMMENT 'User who last updated the record',
-  UPDATE_BATCH_ID           BIGINT                    COMMENT 'Batch ID associated with the update',
-  UPDATE_DATE_TIME          TIMESTAMP_NTZ             COMMENT 'Timestamp when the record was last updated',
+  ACCOUNT_BALANCE_TYPE_ID BIGINT COMMENT 'DB2 Data Type: BIGINT',
+  BALANCE_TYPE_CODE VARCHAR(64) COMMENT 'DB2 Data Type: VARCHAR, Length: 64',
+  BALANCE_TYPE_NAME VARCHAR(128) COMMENT 'DB2 Data Type: VARCHAR, Length: 128',
+  BUSINESS_COLUMN VARCHAR(64) COMMENT 'DB2 Data Type: VARCHAR, Length: 64',
+  CREATED_BY VARCHAR(64) COMMENT 'DB2 Data Type: VARCHAR, Length: 64',
+  CREATE_BATCH_ID BIGINT COMMENT 'DB2 Data Type: BIGINT',
+  CREATE_DATE_TIME TIMESTAMP_NTZ COMMENT 'DB2 Data Type: TIMESTAMP',
+  CURRENT_FLAG CHAR(1) COMMENT 'DB2 Data Type: CHARACTER, Length: 1',
+  DATA_SOURCE VARCHAR(64) COMMENT 'DB2 Data Type: VARCHAR, Length: 64',
+  DESCRIPTION VARCHAR(255) COMMENT 'DB2 Data Type: VARCHAR, Length: 255',
+  EFFECTIVE_FROM TIMESTAMP_NTZ NOT NULL COMMENT 'DB2 Data Type: TIMESTAMP',
+  EFFECTIVE_TO TIMESTAMP_NTZ COMMENT 'DB2 Data Type: TIMESTAMP',
+  IS_DELETED CHAR(1) COMMENT 'DB2 Data Type: CHARACTER, Length: 1',
+  SK_ACCOUNT_BALANCE_TYPE_KEY BIGINT NOT NULL COMMENT 'DB2 Data Type: BIGINT',
+  STATUS_CODE VARCHAR(16) COMMENT 'DB2 Data Type: VARCHAR, Length: 16',
+  UPDATED_BY VARCHAR(64) COMMENT 'DB2 Data Type: VARCHAR, Length: 64',
+  UPDATE_BATCH_ID BIGINT COMMENT 'DB2 Data Type: BIGINT',
+  UPDATE_DATE_TIME TIMESTAMP_NTZ COMMENT 'DB2 Data Type: TIMESTAMP',
 
-  PRIMARY KEY (SK_ACCOUNT_BALANCE_TYPE_KEY)
-)
-COMMENT = 'Dimension table for account balance types';
+  CONSTRAINT PK_DIM_ACCOUNT_BALANCE_TYPE PRIMARY KEY (SK_ACCOUNT_BALANCE_TYPE_KEY)
+);
