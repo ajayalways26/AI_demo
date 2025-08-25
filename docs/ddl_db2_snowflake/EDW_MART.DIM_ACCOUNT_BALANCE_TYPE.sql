d6b293f5921ae80ee4896d1f52ac063c31f5a9b7
ajayalways26 <ajay.singh1@brillio.com>
2025-08-25 12:59:24 +0530

    File EDW_MART.DIM_ACCOUNT_BALANCE_TYPE.sql modified successfully

--- docs/ddl_db2_snowflake/EDW_MART.DIM_ACCOUNT_BALANCE_TYPE.sql
+++ docs/ddl_db2_snowflake/EDW_MART.DIM_ACCOUNT_BALANCE_TYPE.sql
@@ -1,22 +1,22 @@
 CREATE OR REPLACE TABLE adam_data_engg_fsi_demo.EDW_MART.DIM_ACCOUNT_BALANCE_TYPE (
-    ACCOUNT_BALANCE_TYPE_ID NUMBER(38,0), -- Account Balance Type ID
-    BALANCE_TYPE_CODE       STRING, -- Balance Type Code
-    BALANCE_TYPE_NAME       STRING, -- Balance Type Name
-    BUSINESS_COLUMN         STRING, -- Business Column
-    CREATED_BY              STRING, -- Created By
-    CREATE_BATCH_ID         NUMBER(38,0), -- Create Batch ID
-    CREATE_DATE_TIME        TIMESTAMP_NTZ, -- Create Date Time
-    CURRENT_FLAG            STRING, -- Current Flag
-    DATA_SOURCE             STRING, -- Data Source
-    DESCRIPTION             STRING, -- Description
-    EFFECTIVE_FROM          TIMESTAMP_NTZ NOT NULL, -- Effective From
-    EFFECTIVE_TO            TIMESTAMP_NTZ, -- Effective To
-    IS_DELETED              STRING, -- Is Deleted
-    SK_ACCOUNT_BALANCE_TYPE_KEY NUMBER(38,0) NOT NULL, -- Surrogate Key for Account Balance Type
-    STATUS_CODE             STRING, -- Status Code
-    UPDATED_BY              STRING, -- Updated By
-    UPDATE_BATCH_ID         NUMBER(38,0), -- Update Batch ID
-    UPDATE_DATE_TIME        TIMESTAMP_NTZ, -- Update Date Time
+    ACCOUNT_BALANCE_TYPE_ID BIGINT COMMENT 'Unique identifier for account balance type',
+    BALANCE_TYPE_CODE VARCHAR(64) COMMENT 'Code for balance type',
+    BALANCE_TYPE_NAME VARCHAR(128) COMMENT 'Name of the balance type',
+    BUSINESS_COLUMN VARCHAR(64) COMMENT 'Business-related column',
+    CREATED_BY VARCHAR(64) COMMENT 'User who created the record',
+    CREATE_BATCH_ID BIGINT COMMENT 'Batch ID during creation',
+    CREATE_DATE_TIME TIMESTAMP_NTZ COMMENT 'Date and time of record creation',
+    CURRENT_FLAG CHAR(1) COMMENT 'Active status flag (Y/N)',
+    DATA_SOURCE VARCHAR(64) COMMENT 'Source of the data',
+    DESCRIPTION VARCHAR(255) COMMENT 'Detailed description of the account balance type',
+    EFFECTIVE_FROM TIMESTAMP_NTZ NOT NULL COMMENT 'Effective start date',
+    EFFECTIVE_TO TIMESTAMP_NTZ COMMENT 'Effective end date',
+    IS_DELETED CHAR(1) COMMENT 'Deletion status flag (Y/N)',
+    SK_ACCOUNT_BALANCE_TYPE_KEY BIGINT NOT NULL COMMENT 'Surrogate key for account balance type',
+    STATUS_CODE VARCHAR(16) COMMENT 'Status code for record',
+    UPDATED_BY VARCHAR(64) COMMENT 'User who updated the record',
+    UPDATE_BATCH_ID BIGINT COMMENT 'Batch ID during update',
+    UPDATE_DATE_TIME TIMESTAMP_NTZ COMMENT 'Date and time of record update',
 
     CONSTRAINT PK_DIM_ACCOUNT_BALANCE_TYPE PRIMARY KEY (SK_ACCOUNT_BALANCE_TYPE_KEY)
 )
