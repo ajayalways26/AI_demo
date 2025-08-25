40d9570592c27627eaf713b8169f07c637001c04
ajayalways26 <ajay.singh1@brillio.com>
2025-08-25 12:55:24 +0530

    File EDW_MART.DIM_ACCOUNT.sql modified successfully

--- docs/ddl_db2_snowflake/EDW_MART.DIM_ACCOUNT.sql
+++ docs/ddl_db2_snowflake/EDW_MART.DIM_ACCOUNT.sql
@@ -1,31 +1,30 @@
 CREATE OR REPLACE TABLE adam_data_engg_fsi_demo.EDW_MART.DIM_ACCOUNT (
-    ACCOUNT_ID VARCHAR(64), -- Account unique identifier
-    ACCOUNT_NAME VARCHAR(128), -- Account name
-    ACCOUNT_NUMBER VARCHAR(64), -- Account number
-    ACCOUNT_TYPE_ID BIGINT, -- Associated account type identifier
-    BUSINESS_COLUMN VARCHAR(64), -- Business-specific column value
-    CLOSE_DATE DATE, -- Date when the account was closed
-    CREATED_BY VARCHAR(64), -- User who created the record
-    CREATE_BATCH_ID BIGINT, -- Batch process identifier for record creation
-    CREATE_DATE_TIME TIMESTAMP_NTZ, -- Timestamp of record creation
-    CURRENCY_CODE VARCHAR(64), -- Currency code associated with the account
-    CURRENT_FLAG CHAR(1), -- Flag to indicate current record ('Y' or 'N')
-    CUSTOMER_ID BIGINT, -- Associated customer identifier
-    DATA_SOURCE VARCHAR(64), -- Source of data
-    EFFECTIVE_FROM TIMESTAMP_NTZ NOT NULL, -- Effective start timestamp
-    EFFECTIVE_TO TIMESTAMP_NTZ, -- Effective end timestamp
-    IS_DELETED CHAR(1), -- Flag to indicate deletion ('Y' or 'N')
-    OPEN_DATE DATE, -- Date when the account was opened
-    PRODUCT_ID BIGINT, -- Associated product identifier
-    SK_ACCOUNT_KEY BIGINT NOT NULL, -- Surrogate key for account, primary identifier
-    SK_ACCOUNT_STATUS_KEY BIGINT, -- Surrogate key for account status
-    SK_ACCOUNT_TYPE_KEY BIGINT, -- Surrogate key for account type
-    SK_CUSTOMER_KEY BIGINT, -- Surrogate key for customer
-    SK_PRODUCT_KEY BIGINT, -- Surrogate key for product
-    STATUS_ID BIGINT, -- Associated status identifier
-    UPDATED_BY VARCHAR(64), -- User who last updated the record
-    UPDATE_BATCH_ID BIGINT, -- Batch process identifier for record update
-    UPDATE_DATE_TIME TIMESTAMP_NTZ, -- Timestamp of last record update
-  
+    ACCOUNT_ID VARCHAR(64) COMMENT 'Account Identifier',
+    ACCOUNT_NAME VARCHAR(128) COMMENT 'Name of the Account',
+    ACCOUNT_NUMBER VARCHAR(64) COMMENT 'Account Number',
+    ACCOUNT_TYPE_ID BIGINT COMMENT 'Foreign Key to Account Type',
+    BUSINESS_COLUMN VARCHAR(64) COMMENT 'Business Related Information',
+    CLOSE_DATE DATE COMMENT 'Account Close Date',
+    CREATED_BY VARCHAR(64) COMMENT 'User who created the record',
+    CREATE_BATCH_ID BIGINT COMMENT 'Batch ID for creation process',
+    CREATE_DATE_TIME TIMESTAMP_NTZ COMMENT 'Record creation date and time',
+    CURRENCY_CODE VARCHAR(64) COMMENT 'Currency Code',
+    CURRENT_FLAG CHAR(1) COMMENT 'Current status flag',
+    CUSTOMER_ID BIGINT COMMENT 'Foreign Key to Customer',
+    DATA_SOURCE VARCHAR(64) COMMENT 'Source System of the Data',
+    EFFECTIVE_FROM TIMESTAMP_NTZ NOT NULL COMMENT 'Effective date from',
+    EFFECTIVE_TO TIMESTAMP_NTZ COMMENT 'Effective date to',
+    IS_DELETED CHAR(1) COMMENT 'Indicates if the record is deleted',
+    OPEN_DATE DATE COMMENT 'Account Open Date',
+    PRODUCT_ID BIGINT COMMENT 'Foreign Key to Product',
+    SK_ACCOUNT_KEY BIGINT NOT NULL COMMENT 'Surrogate Key for Account',
+    SK_ACCOUNT_STATUS_KEY BIGINT COMMENT 'Surrogate Key for Account Status',
+    SK_ACCOUNT_TYPE_KEY BIGINT COMMENT 'Surrogate Key for Account Type',
+    SK_CUSTOMER_KEY BIGINT COMMENT 'Surrogate Key for Customer',
+    SK_PRODUCT_KEY BIGINT COMMENT 'Surrogate Key for Product',
+    STATUS_ID BIGINT COMMENT 'Status Identifier',
+    UPDATED_BY VARCHAR(64) COMMENT 'User who updated the record',
+    UPDATE_BATCH_ID BIGINT COMMENT 'Batch ID for update process',
+    UPDATE_DATE_TIME TIMESTAMP_NTZ COMMENT 'Record update date and time',
     CONSTRAINT PK_DIM_ACCOUNT PRIMARY KEY (SK_ACCOUNT_KEY)
-)
+);
